958ec2d7146c27b802ee695a7c5d3d138e7320d3
kimarhenry <kimarhenry@yahoo.com>
2016-04-21 00:45:11 -0500

    fix

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = '@localhost/wishlist'
+app.config['SQLALCHEMY_DATABASE_URI'] = '620068542@localhost/wishlist'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
 db = SQLAlchemy(app)
 
